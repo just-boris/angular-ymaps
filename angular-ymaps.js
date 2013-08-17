255e076f4c1b15af06d037cc04d144ec8cd87d38
boris-asus <just-boris@hotmail.com>
2013-08-18 00:16:18 +0400

    положил загрузчик скриптов прямо в модуль, чтобы не таскать лишний файл

--- angular-ymaps.js
+++ angular-ymaps.js
@@ -1,4 +1,50 @@
-angular.module('ymaps', ['script'])
+angular.module('ymaps', [])
+.factory('$script', ['$q', '$rootScope', function ($q, $rootScope) {
+    "use strict";
+    //классический кроссбраузерный способ подключить внешний скрипт
+    function loadScript(path, callback) {
+        var el = document.createElement("script");
+        el.onload = el.onreadystatechange = function () {
+            if (el.readyState && el.readyState !== "complete" &&
+                el.readyState !== "loaded") {
+                return false;
+            }
+            // если все загрузилось, то снимаем обработчик и выбрасываем callback
+            el.onload = el.onreadystatechange = null;
+            if(angular.isFunction(callback)) {
+                callback();
+            }
+        };
+        el.async = true;
+        el.src = path;
+        document.getElementsByTagName('body')[0].appendChild(el);
+    }
+    var loadHistory = [], //кэш загруженных файлов
+        pendingPromises = {}; //обещания на текущие загруки
+    return {
+        get: function(url) {
+            var deferred = $q.defer();
+            if(loadHistory.indexOf(url) !== -1) {
+                deferred.resolve();
+            }
+            else if(pendingPromises[url]) {
+                return pendingPromises[url];
+            } else {
+                loadScript(url, function() {
+                    delete pendingPromises[url];
+                    loadHistory.push(url);
+                    //обязательно использовать `$apply`, чтобы сообщить
+                    //angular о том, что что-то произошло
+                    $rootScope.$apply(function() {
+                        deferred.resolve();
+                    });
+                });
+                pendingPromises[url] = deferred.promise;
+            }
+            return deferred.promise;
+        }
+    };
+}])
 .constant('ymapConfig', {
     mapBehaviors: ['default'],
     markerOptions: {
