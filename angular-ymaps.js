bb80135e3ecdb4fb87b88724b0f7a32bc1f6a0f3
Sergey Moiseev <moiseev.sergey@gmail.com>
2014-06-05 04:00:52 +0400

    replace compile with link and add transclude

--- angular-ymaps.js
+++ angular-ymaps.js
@@ -153,25 +153,17 @@ angular.module('ymaps', [])
     "use strict";
     return {
         restrict: 'EA',
+        transclude: true,
+        replace: false,
+        template: "<div class=\"angular-ymaps\"><div ng-transclude style=\"display: none\"></div></div>",
         scope: {
             center: '=',
             zoom: '='
         },
-        compile: function(tElement) {
-            var childNodes = tElement.html();
-            tElement.html('');
-            return function($scope, element) {
-                ymapsLoader.ready(function() {
-                    childNodes = angular.element('<div></div>').html(childNodes).contents();
-                    element.append(childNodes);
-                    $compile(childNodes)($scope.$parent);
-                });
-            };
-        },
         controller: 'YmapController'
     };
 }])
-.directive('ymapMarker', function () {
+.directive('ymapMarker', ['ymapsLoader', function (ymapsLoader) {
     "use strict";
     return {
         restrict: "EA",
@@ -184,34 +176,36 @@ angular.module('ymaps', [])
         },
         link    : function ($scope, elm, attr, mapCtrl) {
             var marker;
-            function pickMarker() {
-                var coord = [
-                    parseFloat($scope.coordinates[0]),
-                    parseFloat($scope.coordinates[1])
-                ];
-                if (marker) {
-                    marker.geometry.setCoordinates(coord);
+            ymapsLoader.ready(function(ymaps) {
+                function pickMarker() {
+                    var coord = [
+                        parseFloat($scope.coordinates[0]),
+                        parseFloat($scope.coordinates[1])
+                    ];
+                    if (marker) {
+                        marker.geometry.setCoordinates(coord);
+                    }
+                    else {
+                        marker = mapCtrl.addMarker(coord, angular.extend({iconContent: $scope.index}, $scope.properties), $scope.options);
+                    }
                 }
-                else {
-                    marker = mapCtrl.addMarker(coord, angular.extend({iconContent: $scope.index}, $scope.properties), $scope.options);
-                }
-            }
 
-            $scope.$watch("index", function (newVal) {
-                if (marker) {
-                    marker.properties.set('iconContent', newVal);
-                }
-            });
-            $scope.$watch("coordinates", function (newVal) {
-                if (newVal) {
-                    pickMarker();
-                }
-            }, true);
-            $scope.$on('$destroy', function () {
-                if (marker) {
-                    mapCtrl.removeMarker(marker);
-                }
+                $scope.$watch("index", function (newVal) {
+                    if (marker) {
+                        marker.properties.set('iconContent', newVal);
+                    }
+                });
+                $scope.$watch("coordinates", function (newVal) {
+                    if (newVal) {
+                        pickMarker();
+                    }
+                }, true);
+                $scope.$on('$destroy', function () {
+                    if (marker) {
+                        mapCtrl.removeMarker(marker);
+                    }
+                });
             });
         }
     };
-});
+}]);
