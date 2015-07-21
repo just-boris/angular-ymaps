c869645802925bb783f10abdda4352eee4fc29c0
just-boris <just-boris@yandex-team.ru>
2015-07-21 12:49:51 +0300

    Built angular-ymaps from commit d544e77 on branch master

--- examples/multi-map/script.js
+++ examples/multi-map/script.js
@@ -6,11 +6,11 @@ angular.module('MyApp', ['ymaps']).controller('MapCtrl', function($scope) {
     $scope.rightMap = {
         center: [59.93, 30.33], zoom: 12
     };
-    $scope.$watch('bindMaps', function(bindMaps) {
-        if(bindMaps) {
+    $scope.updateBinding = function() {
+        if($scope.bindMaps) {
             $scope.rightMap = $scope.leftMap;
         } else {
             $scope.rightMap = angular.copy($scope.leftMap);
         }
-    });
+    };
 });
